0f6ca065e2e6f2ea1c25266ef62943be37d7276a
Bhargavi <kavisubramani2008@gmail.com>
2026-03-08 00:11:31 +0530

    increment and decrement

---
+++ program4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=100,b=95;
+    float c=100,d=99;
+
+    printf(" ++a %d\n", ++a);
+    printf(" ++b %d\n", ++b);
+    printf(" --c %.0f\n", --c);
+    printf(" --d %.0f\n", --d);
+    return 0;
+}
